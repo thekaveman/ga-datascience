aff94d6e1155dfc3c68bed568de7ee885f7a6321
thekaveman <kegan.maher@gmail.com>
2015-10-18 13:39:12 -0700

    fill missing values in a column

---
+++ fillMissing.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# import the titanic data into DataFrame
+data = pd.read_csv("./data/train.csv")
+
+# what does the data look like before filling?
+print data.info()
+
+# fill with the average age (overwriting the current Age column)
+avg_age = data.Age.mean()
+data.Age = data.Age.fillna(avg_age)
+
+# look at the data again after filling to ensure it didn't significant alter
+print data.info()
