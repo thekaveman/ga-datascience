6ff25a69edc2d832463000138100543d2c20344c
thekaveman <kegan.maher@gmail.com>
2015-10-18 11:00:27 -0700

    close the connection when done

--- sqlImportExport.py
+++ sqlImportExport.py
@@ -2,9 +2,14 @@ import pandas as pd
 import sqlite3
 
 # establish a connection to the sqlite db
+
 conn = sqlite3.connect("./data/test_pandas.db")
 
 # query the database and load into DataFrame
 
 query = "SELECT * FROM test"
 data = pd.read_sql(query, conn)
+
+# close the connection to the db
+
+conn.close()
