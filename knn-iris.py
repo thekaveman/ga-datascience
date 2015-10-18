b70c94abbba1e825d44f9485694b5667e3408025
thekaveman <kegan.maher@gmail.com>
2015-10-18 15:01:25 -0700

    converting to numpy arrays for input to scikit-learn

--- knn-iris.py
+++ knn-iris.py
@@ -1,4 +1,17 @@
 import pandas as pd
 
 # import the iris dataset into a DataFrame
-data = pd.read_csv("./data/iris.csv")
+iris = pd.read_csv("./data/iris.csv")
+
+# exploring the data
+print iris.head()
+print iris.info()
+print iris.describe()
+
+# create two variables, for features and target -> convert to values
+iris_features = iris[["sepal_length",
+                      "sepal_width",
+                      "petal_length",
+                      "petal_width"]].values
+
+iris_target = iris.species.values
