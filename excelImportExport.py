ac663355a7ec46115b55eb3fda20fd0c9533bf1a
thekaveman <kegan.maher@gmail.com>
2015-10-18 11:03:29 -0700

    write DataFrame to excel file

--- excelImportExport.py
+++ excelImportExport.py
@@ -3,3 +3,10 @@ import pandas as pd
 # loading DataFrame from an Excel file
 
 excelData = pd.read_excel("./data/test_pandas.xlsm", "Sheet1")
+
+# writing DataFrame to an Excel file
+
+writer = pd.ExcelWriter("./data/test_sheets.xlsx")
+data.to_excel(writer, "Original")
+data.to_excel(writer, "Copy")
+writer.save()
