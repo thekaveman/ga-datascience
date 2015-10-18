e943c74fae9f4d23118a9110276932c3b98e780b
thekaveman <kegan.maher@gmail.com>
2015-10-18 15:47:27 -0700

    splitting data into train/test sets

--- exercise2.py
+++ exercise2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.cross_validation import train_test_split
 
 # loading our cleaned data
 data = pd.read_csv("./data/clean_data.csv")
@@ -7,3 +8,11 @@ data = pd.read_csv("./data/clean_data.csv")
 print data.head()
 print data.info()
 print data.describe()
+
+# getting the features and target sets as numpy arrays
+passenger_features = data[["Age", "Sex", "Pclass"]].values
+passenger_target = data.Survived.values
+
+# splitting into train/test sets
+features_train, features_test, target_train, target_test = \
+    train_test_split(passenger_features, passenger_target, test_size=0.20)
