b1cb0ba41a14b526918e82065f5ca5aed6c8a5e8
thekaveman <kegan.maher@gmail.com>
2015-10-18 15:35:19 -0700

    iterating on the train/test split

--- knn-iris.py
+++ knn-iris.py
@@ -18,14 +18,19 @@ iris_features = iris[["sepal_length",
 
 iris_target = iris.species.values
 
-# splitting the iris features and target into train and test sets
-features_train, features_test, target_train, target_test = \
-    train_test_split(iris_features, iris_target, test_size=0.20, random_state=0)
-
 # trying different values of K
 for k in range(2,8):
-    # training a KNN classifier
-    model = KNeighborsClassifier(k).fit(features_train, target_train)
-    # get an accuracy score of the model with this value of k
-    accuracy = model.score(features_test, target_test)
-    print "k = {}, accuracy = {}".format(k, accuracy)
+    score = 0
+    # simulating the shuffling N times
+    N = 10
+    for x in range(N):
+        # splitting the iris features and target into train and test sets
+        features_train, features_test, target_train, target_test = \
+            train_test_split(iris_features, iris_target, test_size=0.20)
+        # training a KNN classifier
+        model = KNeighborsClassifier(k).fit(features_train, target_train)
+        # get an accuracy score of the model with this value of k
+        score = score + model.score(features_test, target_test)
+    # get an average score for these N iterations 
+    score = score / N
+    print "k = {}, score = {}".format(k, score)
