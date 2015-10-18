200c1f0d6063351a70e977ad27751eeb467876bb
thekaveman <kegan.maher@gmail.com>
2015-10-18 10:56:18 -0700

    split import/export

--- csvImportExport.py
+++ csvImportExport.py
@@ -7,7 +7,3 @@ csvData = pd.read_csv("./data/test_pandas.csv", header=0)
 # writing DataFrame to CSV file
 
 csvData.to_csv("./data/test_data.csv", header=True, index=False, sep="|")
-
-# loading DataFrame from an Excel file
-
-excelData = pd.read_excel("./data/test_pandas.xlsm", "Sheet1")
---
+++ excelImportExport.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# loading DataFrame from an Excel file
+
+excelData = pd.read_excel("./data/test_pandas.xlsm", "Sheet1")
