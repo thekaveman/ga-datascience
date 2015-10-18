e4fe75ea6e4aa8a45f0954028d232001a82bc89a
thekaveman <kegan.maher@gmail.com>
2015-10-18 10:53:23 -0700

    reading Excel into DataFrame

--- importExport.py
+++ importExport.py
@@ -2,8 +2,12 @@ import pandas as pd
 
 # loading DataFrame from a CSV file
 
-data = pd.read_csv("./data/test_pandas.csv", header=0)
+csvData = pd.read_csv("./data/test_pandas.csv", header=0)
 
 # writing DataFrame to CSV file
 
-data.to_csv("./data/test_data.csv", header=True, index=False, sep="|")
+csvData.to_csv("./data/test_data.csv", header=True, index=False, sep="|")
+
+# loading DataFrame from an Excel file
+
+excelData = pd.read_excel("./data/test_pandas.xlsm", "Sheet1")
